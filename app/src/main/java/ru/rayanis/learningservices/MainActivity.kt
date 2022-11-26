6d4d1dfb2faf099a0ed1252cd9f874dae88ae786
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2022-11-26 16:48:58 +0300

    Initial commit

---
+++ app/src/main/java/ru/rayanis/learningservices/MainActivity.kt
@@ -0,0 +1,17 @@
+package ru.rayanis.learningservices
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.rayanis.learningservices.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val b by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(b.root)
+    }
+}
