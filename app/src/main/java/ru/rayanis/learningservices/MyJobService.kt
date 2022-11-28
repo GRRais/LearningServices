767d287c90eb0b76674a0dd6d98eccc0362a334f
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2022-11-28 16:21:18 +0300

    finished lesson Job Service create, ready to lesson Job Service start

---
+++ app/src/main/java/ru/rayanis/learningservices/MyJobService.kt
@@ -0,0 +1,46 @@
+package ru.rayanis.learningservices
+
+import android.app.Service
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.*
+
+class MyJobService : JobService() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
+        log("onDestroy")
+    }
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        log("onStartCommand")
+        coroutineScope.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer $i")
+            }
+            jobFinished(params, true)
+        }
+        return true
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        log("onStopJob")
+        return true
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG" , "MyService: $message")
+    }
+}
